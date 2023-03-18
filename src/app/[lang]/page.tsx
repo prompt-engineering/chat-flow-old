476c1ecefa29d8e729ff3c334ea40b3f6093afcf
Phodal Huang <h@phodal.com>
2023-03-18 20:21:13 +0800

    fix: fix action

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -10,41 +10,41 @@ async function Page() {
   const t = i18n.tFactory("/");
 
   return (
-    <Box mb={ 20 }>
-      <Box as='section' pt='6rem' pb={ { base: "0", md: "5rem" } }>
+    <Box mb={20}>
+      <Box as='section' pt='6rem' pb={{ base: "0", md: "5rem" }}>
         <Container>
           <Box textAlign='center'>
-            <ClickPromptHome width={ 128 } height={ 128 }/>
+            <ClickPromptHome width={128} height={128} />
             <Heading
               as='h1'
               maxW='16ch'
               mx='auto'
-              fontSize={ { base: "2.25rem", sm: "3rem", lg: "4rem" } }
+              fontSize={{ base: "2.25rem", sm: "3rem", lg: "4rem" }}
               fontFamily='heading'
               letterSpacing='tighter'
               fontWeight='extrabold'
               mb='16px'
               lineHeight='1.2'
             >
-              { t("title") }
+              {t("title")}
             </Heading>
 
             <Text
               maxW='560px'
               mx='auto'
               color='gray.500'
-              _dark={ { color: "gray.400" } }
-              fontSize={ { base: "lg", lg: "xl" } }
+              _dark={{ color: "gray.400" }}
+              fontSize={{ base: "lg", lg: "xl" }}
               mt='6'
             >
-              { t("description") }
+              {t("description")}
             </Text>
 
-            <Stack mt='10' spacing='4' justify='center' direction={ { base: "column", sm: "row" } }>
-              <Button as='a' size='lg' h='4rem' px='40px' fontSize='1.2rem' href={ GITHUB_URL } target='__blank'>
+            <Stack mt='10' spacing='4' justify='center' direction={{ base: "column", sm: "row" }}>
+              <Button as='a' size='lg' h='4rem' px='40px' fontSize='1.2rem' href={GITHUB_URL} target='__blank'>
                 GitHub
               </Button>
-              <ClickPromptButton size='lg' h='4rem' px='40px' text='Hello World'/>
+              <ClickPromptButton size='lg' h='4rem' px='40px' text='Hello World' />
             </Stack>
           </Box>
         </Container>
