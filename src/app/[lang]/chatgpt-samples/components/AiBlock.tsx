a5c67c1cc30bde9235786a5ccf616dc9f5d05900
Phodal Huang <h@phodal.com>
2023-03-18 20:14:17 +0800

    refactor: remove old codes

--- src/app/[lang]/chatgpt-samples/components/AiBlock.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import styled from "@emotion/styled";
-import { Flex } from "@/components/ChakraUI";
-
-export const AiBlock = styled(Flex)`
-  background-color: #fff;
-  padding: 1rem;
-`;
